d146191ed7883b7efaac9d5815cd9ba83de84030
planetdoy <doydoit@gmail.com>
2021-08-04 18:03:18 +0900

    유틸리티 객체와 날짜

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpSession;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -45,7 +46,7 @@ public class BasicController {
         model.addAttribute("user", userA);
         model.addAttribute("users", list);
         model.addAttribute("userMap", map);
-        
+
         return "basic/variable";
     }
 
@@ -54,6 +55,13 @@ public class BasicController {
         session.setAttribute("sessionData", "Hello Session");
         return "basic/basic-objects";
     }
+
+    @GetMapping("/date")
+    public String date(Model model) {
+        model.addAttribute("localDateTime", LocalDateTime.now());
+        return "basic/date";
+    }
+
 
     @Component("helloBean")
     static class HelloBean {
